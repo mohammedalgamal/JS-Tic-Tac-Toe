2429bd161bfdf06d22c82acfaae11f6287147cc7
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-12-04 19:32:37 +0200

    Add functionality to allow players to put markers

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const global = (() => {
     const gameBoard = (() => {
         
-        const board = ['x', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+        const board = ['', '', '', '', '', '', '', '', ''];
         
         const makeBoard = (() => {
             let container = document.querySelector('.board');
-            for (let i = 1; i <= 9; i++) {
+            for (let i = 0; i < board.length; i++) {
                 let temp_div = document.createElement('div');
                 temp_div.className = `place${i}`;
                 temp_div.classList += ' cell';
@@ -13,26 +13,50 @@ const global = (() => {
             };
         })();
 
-        const displayBoard = ((board) => {
+        const displayBoard = (board) => {
             let cells = document.querySelectorAll('div[class^=place]');
-            for (let i = 0; i < board.length; i++) {
+            for(let i = 0; i < board.length; i++) {
                 cells[i].innerHTML = board[i];
             }
-        })(board);
+        };
+        displayBoard(board);
 
-        return {board};
+        return {board, displayBoard};
     })();
 
     const game = (() => {
-
+        const fillBoard = (() => {
+            let cells = document.querySelectorAll('div[class^=place]');
+            let announce = document.querySelector('.announcer');
+            for(let i = 0; i < gameBoard.board.length; i++) {
+                cells[i].addEventListener('click', () => {
+                    if (gameBoard.board[i] == '') {
+                        if(x_player.myTurn) {
+                            gameBoard.board[i] = x_player.playerSymbol;
+                            gameBoard.displayBoard(gameBoard.board);
+                            x_player.myTurn = false;
+                            o_player.myTurn = true;
+                            announce.innerHTML = "Player O's turn";
+                        }
+                        else {
+                            gameBoard.board[i] = o_player.playerSymbol;
+                            gameBoard.displayBoard(gameBoard.board);
+                            x_player.myTurn = true;
+                            o_player.myTurn = false;
+                            announce.innerHTML = "Player X's turn";
+                        };
+                    };
+                });
+            };
+        })();
     })();
 
-    const player = (playerSymbol) => {
-        return {playerSymbol};
+    const player = (playerSymbol, myTurn) => {
+        return {playerSymbol, myTurn};
     };
 
-    const x_player = player('x');
-    const o_player = player('o');
+    const x_player = player('X', true);
+    const o_player = player('O', false);
     
     return {gameBoard, game, x_player, o_player};
 })();
