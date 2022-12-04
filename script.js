596716f710be73d076183d0f09189fb5b62f73be
mohammedalgamal <mohammedalgamal95@gmail.com>
2022-12-04 16:15:07 +0200

    Add board outlines and style restart button

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const global = (() => {
     const gameBoard = (() => {
         const board = ['x', 'o', 'x', 'o', 'x', 'o', 'x', 'o', 'x'];
+        const makeBoard = (() => {
+            let container = document.querySelector('.board');
+            for (let i = 1; i <= 9; i++) {
+                let temp_div = document.createElement('div');
+                temp_div.className = `place${i}`;
+                temp_div.classList += ' cell';
+                container.appendChild(temp_div);
+            };
+        })();
         return {board};
     })();
 
